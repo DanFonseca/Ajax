c0c202d5ce175e8ddda5247bfd474c2ce274c3c7
danie <daniel.freitas.ms1@gmail.com>
2020-06-27 12:46:55 -0300

    upload de arquivo e melhoria no express

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 let express= require ('express')
-let bodyParser = require('body-parser')
+let multer = require('multer')
+
 let app = express()
 
 app.use(express.static('.'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+const porta = 8080
+
+let storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback){
+        callback(null, `${file.fieldname}-${Date.now()}`)
+    }
+})
+
+let upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err =>{
+        if(err){
+            return res.send('Ocorreu um erro')
+        }
+        res.send('Concluído com Sucesso!')
+    })
+})
 
 
-app.listen(8080, () => console.log('executando..'))
+app.listen(porta, () => console.log('executando..'))
