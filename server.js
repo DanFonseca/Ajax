5f40b054679d13096c3d11f2d28bd54af3f0eeed
danie <daniel.freitas.ms1@gmail.com>
2020-06-26 23:50:24 -0300

    Criando uma requisicao XMLHttpRequest_pt1

--- server.js
+++ server.js
@@ -6,6 +6,5 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json)
 
-app.get('/test', (req, res) => res.send('Ok'))
 
-app.listen(8080)
+app.listen(8080, () => console.log('executando..'))
